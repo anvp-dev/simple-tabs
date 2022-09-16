9362a4995722de8da4fee5264b11546866b04f3f
Andrey Pavlenko <102993820+anvp-dev@users.noreply.github.com>
2022-09-17 01:54:42 +0500

    Add files via upload

--- tabs.js
+++ tabs.js
@@ -5,9 +5,9 @@ const tab = (selector) => {
   content.forEach((item, index) => index === 0 ? true : item.style.display = 'none')
 
   btn.forEach((item, index) => item.addEventListener("click", () => {
-    btn.forEach(btn => btn.classList.remove('active'))
+    btn.forEach(btn => btn.classList.remove('tab-btn-active'))
     content.forEach(content => content.style.display = 'none')
-    item.classList.add('active');
+    item.classList.add('tab-btn-active');
     content[index].style.display = 'block';
   }));
 };
